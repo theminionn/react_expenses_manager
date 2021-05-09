f5cf4aa9fbc25702796e89e3ee5e14b34911510a
Yordanka Ivanova <dani@Yordankas-MacBook-Pro.local>
2021-05-09 01:19:09 +0100

    add code to filter out expenses based on filter year

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-  const [enteredYear, setEnteredYear] = useState('');
-
-  const yearChangeHandler = (event) => {
+  const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
   };
 
@@ -13,7 +11,7 @@ const ExpensesFilter = (props) => {
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select value={props.selected} onChange={yearChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
